4b33ad1b47cc8ad8b07952c4a1839bf6e78fedd5
mutyala srinivas <srinivasmutyala54@gmail.com>
2023-12-08 12:31:18 +0530

    add loaction of expenditure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
  import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
     <div className="App">
       <>
       <h1>Expense Items</h1>
-      <ul>
-        <li>Food Rs 10</li>
-        <li>Petrol Rs 100</li>
-        <li>Movies Rs 200</li>
-      </ul>
+       <ExpenseItem name="Food" price="100" place="Annavaram"/>
+       <ExpenseItem name="Petrol" price="500" place="Amalapuram"/>
+       <ExpenseItem name="Movie" price="700" place="Goa"/>
       </>
     </div>
   );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,11 @@
 
-const ExpenseItem=()=>{
+const ExpenseItem=({name,price,place})=>{
+
     return(
-        <>
-         
-        </>
+        <div className="list">
+         <h5>{name}--{price}</h5>
+         <h5>Location of Expenditure{place}</h5>
+        </div>
     )
 }
 export default ExpenseItem;
