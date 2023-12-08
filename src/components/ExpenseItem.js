31b37fec6c6e12305e97169bf2ea1cccc5df9efa
mutyala srinivas <srinivasmutyala54@gmail.com>
2023-12-08 13:12:38 +0530

    small changes

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,13 @@
 
 const ExpenseItem=({name,price,place})=>{
+    const expenseDate=new Date();
+    const expenseTitle="grocery";
 
     return(
         <div className="list">
          <h5>{name}--{price}</h5>
+         <h5>{expenseTitle}</h5>
+         <h3>{date}</h3>
          <h5>Location of Expenditure{place}</h5>
         </div>
     )
