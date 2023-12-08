e80bba100705398e33e7ef12cf155098f345108b
mutyala srinivas <srinivasmutyala54@gmail.com>
2023-12-08 12:42:48 +0530

    using map

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenses=[{name:"food",price:100,place:"Annavaram"},{name:"petrol",price:900,place:"vadapalli"},{name:"movie",price:500,place:"goa"}]
   return (
     <div className="App">
       <>
       <h1>Expense Items</h1>
-       <ExpenseItem name="Food" price="100" place="Annavaram"/>
-       <ExpenseItem name="Petrol" price="500" place="Amalapuram"/>
-       <ExpenseItem name="Movie" price="700" place="Goa"/>
+       {expenses.map((expense)=><ExpenseItem name={expense.name} price={expense.price} place={expense.place}/>
+       )}
       </>
     </div>
   );
