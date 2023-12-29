4046a8bc5f9182b53289612e1c2f28114b88e186
mutyala srinivas <srinivasmutyala54@gmail.com>
2023-12-29 15:34:30 +0530

    react portals

--- test-project/src/components/UserInput.js
+++ test-project/src/components/UserInput.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
+import Wrapper from './Helpers.js/Wrapper';
+import ErrorModal from './UI/ErrorModal';
 
 
 const UserInput=(props)=>{
     const[userName,setUserName]=useState("")
     const [userAge,setUserAge]=useState("")
+    const [error,setError]=useState();
     const nameHandler=(e)=>{
       setUserName(e.target.value);
     }
@@ -12,13 +15,40 @@ const UserInput=(props)=>{
     }
     const submitHandler=(e)=>{
         e.preventDefault();
+        if(userName.trim().length===0||userAge.trim().length===0){
+          setError({
+            title:"InValid Details ",
+            message:"Enter valid username and Age"
+          })
+          return;
+        }
+        if(+userAge<1){
+          setError({
+            title:"inValid Age",
+            message:"Enter valid Age always age shuold be >0"
+          })
+        }
         const user={
             name:userName,
             age:userAge
         }
         props.userDetails(user)
     }
+    const errorHandler=()=>{
+       setError(null);
+    }
   return (
+    <Wrapper>
+      {
+        error&&
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+
+        />
+      }
+    
     <div className='user-input'>
         <form className='form-container' onSubmit={submitHandler}>
         <label>Username</label>
@@ -29,6 +59,8 @@ const UserInput=(props)=>{
         </form>
          
      </div>
+       
+    </Wrapper>
   )
 }
 
